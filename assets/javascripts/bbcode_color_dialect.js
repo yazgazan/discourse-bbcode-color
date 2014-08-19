fbc6c1b55a4697ac05708e4b1323cef6b20569dc
yazou <yazgazan@gmail.com>
2014-08-19 10:24:01 -0400

    adding support for img and center

--- assets/javascripts/bbcode_color_dialect.js
+++ assets/javascripts/bbcode_color_dialect.js
@@ -10,4 +10,37 @@
       }
     }
   });
+  Discourse.Dialect.inlineBetween({
+    start: "[color]",
+    stop:  "[/color]",
+    rawContents: true,
+    emitter: function(contents) {
+      var matches = contents.match(/](.*)/);
+      if (matches) {
+        return ['img', {src: matches[1]}, ""];
+      }
+    }
+  });
+  Discourse.Dialect.inlineBetween({
+    start: "[color]",
+    stop:  "[/color]",
+    rawContents: true,
+    emitter: function(contents) {
+      var matches = contents.match(/](.*)/);
+      if (matches) {
+        return ['img', {src: matches[1]}, ""];
+      }
+    }
+  });
+  Discourse.Dialect.inlineBetween({
+    start: "[center]",
+    stop:  "[/center]",
+    rawContents: true,
+    emitter: function(contents) {
+      var matches = contents.match(/](.*)/);
+      if (matches) {
+        return ['div', {style: "text-align: center;"}, matches[1]];
+      }
+    }
+  });
 })();
